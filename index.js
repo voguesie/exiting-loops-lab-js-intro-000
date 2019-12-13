356a787932544b4553f9539bc96a75b652e252d8
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-13 04:16:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,5 +24,10 @@ function keepGoing(array, changeValue, skipValue) { // Changes every value to ch
 
 
 function findBy(array, findFn) { // Looks for an element in array based on findFn
-  
+  for (let i = 0; i < array.length; i++) {
+  if (array[i] === findFn()) {
+    return array[i]
+  }
+}
+return null
 }
