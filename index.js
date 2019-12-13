a26bab8c4a5db3e8b45cc8838f4d9c4ba4fe503d
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-13 03:52:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function breakOut(array, changeValue, stopValue) { // Every value changes to changeValue until stopValue is encountered
   for (let i = 0; i < array.length; i++) {
-    if (array[i] !== stopValue) {
+    if (array[i] !== stopValue) { // Checks if element is the stop value. If not, changes to changeValue.
       array[i] = changeValue;
     }
     else {
-      break
+      break // Breaks when encounters changeValue
     }
   }
   return array
